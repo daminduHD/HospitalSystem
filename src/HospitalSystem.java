f88c506f14d898d0923e2456cc5c42985dfe89ae
Damindu Heshan Dissanayake <143378749+daminduHD@users.noreply.github.com>
2026-09-07 20:03:56 +0530

    Implemented patient linked list

--- src/HospitalSystem.java
+++ src/HospitalSystem.java
@@ -18,3 +18,35 @@ public class HospitalSystem {
     public static void main(String[] args) {
     }
 }
+
+class Visit {
+    String visitID, date, doctorName, diagnosis, treatment;
+    Visit next;
+
+    public Visit(String id, String d, String doc, String diag, String treat) {
+        this.visitID = id; this.date = d; this.doctorName = doc; 
+        this.diagnosis = diag; this.treatment = treat; this.next = null;
+    }
+}
+
+class VisitHistoryList {
+    Visit head;
+    
+    public void addVisit(String id, String date, String doc, String diag, String treat) {
+        Visit newVisit = new Visit(id, date, doc, diag, treat);
+        if (head != null) newVisit.next = head;
+        head = newVisit;
+    }
+
+    public String getHistoryDisplay() {
+        if (head == null) return " No previous visits recorded.\n";
+        StringBuilder sb = new StringBuilder();
+        Visit temp = head;
+        while (temp != null) {
+            sb.append(String.format(" - [Visit ID: %s] Date: %s | Dr. %s | %s | %s\n",
+                    temp.visitID, temp.date, temp.doctorName, temp.diagnosis, temp.treatment));
+            temp = temp.next;
+        }
+        return sb.toString();
+    }
+}
